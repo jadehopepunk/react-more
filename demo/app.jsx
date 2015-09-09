3fc3132d733081f09f14a2c87164092e3f9be3f6
Craig Ambrose <craig@craigambrose.com>
2015-09-09 14:40:25 +1200

    sets number of lines

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,6 +1,6 @@
 'use strict';
 
-var React = require('react');
+var React = require('react/addons');
 var Fork = require('react-ghfork');
 
 var readme = require('../README.md');
@@ -9,50 +9,60 @@ var pkgInfo = require('../package.json');
 //var Ellipsify = require('../lib');
 
 module.exports = React.createClass({
-    displayName: 'App',
+  displayName: 'App',
 
-    //mixins: [React.addons.LinkedStateMixin],
+  mixins: [React.addons.LinkedStateMixin],
 
-    //getInitialState() {
-    //    return {
-    //        visible: false,
-    //        visibleItems: 5,
-    //        separator: ' ',
-    //        more: '…',
-    //        atFront: true,
-    //    };
-    //},
+  getInitialState() {
+    return {
+      lines: 2
+    };
+  },
 
-    render() {
-        //var visible = this.state.visible;
-        //var visibleItems = parseInt(this.state.visibleItems, 10);
-        //var separator = this.state.separator;
-        //var more = this.state.more;
-        //var atFront = this.state.atFront;
+  render() {
+    var lines = this.state.lines;
+    //var visibleItems = parseInt(this.state.visibleItems, 10);
+    //var separator = this.state.separator;
+    //var more = this.state.more;
+    //var atFront = this.state.atFront;
 
-        return (
-            <div className='pure-g'>
-                <Fork className='right' project={pkgInfo.user + '/' + pkgInfo.name} />
-                <header className='pure-u-1'>
-                    <h1>{pkgInfo.name}</h1>
+    return (
+      <div className='pure-g'>
+        <Fork className='right' project={pkgInfo.user + '/' + pkgInfo.name}/>
+        <header className='pure-u-1'>
+          <h1>{pkgInfo.name}</h1>
 
-                    <div className='description'>{pkgInfo.description}</div>
-                </header>
-                <article className='pure-u-1'>
-                    <section className='demonstration'>
-                        <div className='description'>
-                            <h2>Demonstration</h2>
+          <div className='description'>{pkgInfo.description}</div>
+        </header>
+        <article className='pure-u-1'>
+          <section className='demonstration'>
+            <div className='description'>
+              <h2>Demonstration</h2>
 
-                            <p>Tweak values below and see how Ellipsify behaves.</p>
-                        </div>
-                    </section>
-                    <section className='documentation'>
-                        <h2>README</h2>
+              <p>Tweak values below and see how Ellipsify behaves.</p>
 
-                        <div dangerouslySetInnerHTML={{__html: readme}}></div>
-                    </section>
-                </article>
+              <form className='pure-form pure-form-stacked controls'>
+                <fieldset>
+                  <div className='pure-g'>
+                    <div className='pure-u-1-2 pure-u-md-1-2'>
+                      <label>Number of Lines</label>
+                      <input type='number' className='pure-u-23-24' valueLink={this.linkState('lines')}/>
+                    </div>
+                  </div>
+                </fieldset>
+
+              </form>
             </div>
-        );
-    }
+          </section>
+          <section className='documentation'>
+            <h2>README</h2>
+
+            <div dangerouslySetInnerHTML={{__html: readme}}></div>
+          </section>
+        </article>
+      </div>
+    );
+  },
+
+
 });
